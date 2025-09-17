e28a5fa04bbad37cdba01d42dc2d391e30c59d8d
Yusupov_dev <yusupovabdulaziz222@gmail.com>
2025-09-17 23:08:55 +0500

    created little project using webpack

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css"
+
+export default function App() {
+    return (
+        <div className="container">
+            <h1 className="title">Hello from App.jsx </h1>
+            <p className="subtitle">Welcome to your first styled React app </p>
+            <button className="btn">Click Me</button>
+        </div>
+    );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,43 @@
-const path = require("path")
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        dirname: path.resolve(__dirname, "dist")
-    }
-}
+  mode: "development",
+  entry: "./src/index.jsx",
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+};
